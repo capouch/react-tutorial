516d03a413f358762071ea5080da3bd528b90a2c
capouch <brianc@palaver.net>
2015-11-28 17:25:17 -0500

    Use socket.io to post new entry

--- server.js
+++ server.js
@@ -77,6 +77,31 @@ io.sockets.on('connection', function (client) {
     client.emit('update', JSON.parse(data));
     });
   });
+  client.on('submission', function (entry) {
+    fs.readFile(COMMENTS_FILE, function(err, data) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      var comments = JSON.parse(data);
+      // NOTE: In a real implementation, we would likely rely on a database or
+      // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
+      // treat Date.now() as unique-enough for our purposes.
+      var newComment = {
+        id: Date.now(),
+        author: req.body.author,
+        text: req.body.text,
+      };
+      comments.push(newComment);
+      fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        }
+        client.emit('update', JSON.parse(comments));
+      });
+    });
+  });
 });
 
 server.listen(3000);
